f718310cc05c0fafee2f4aa550c5a0cc7fd03c69
Rok Ragnarok <yoekzumaya@gmail.com>
2022-03-18 13:35:11 -0600

    function calculatetext

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -4,20 +4,62 @@ class Game extends Component {
     constructor(){
         super()
         this.state={
-number: 0,
-message: "",
+number: "",
+message: "hola que hace",
 random: generateRandomNumber(100)
         }
     }
+handleOnChange = e => {
+    //const value = e.target.value
+    const {target :{value}} = e;
+    console.log(value);
+if(value.trim() > 0 ){
+
+this.setState({
+    number: value,
+});
+
+}
+handleOnClick = () =>{
+    const number = parseInt.state.number;
+
+    const random =parseInt.state.random;
+    console.log(random);
+    const text = calculateText(number, random);
+
+    this.setState({
+        message: text
+    })
+}
+
+    event.target.value
+
+const event ={
+    name: "Rok",
+    target: {
+value: 34
+
+    }
+
+
+}
+
+
+
+
+
+    console.log(value);
+}
+
     render() {
         return (
             <div className ="Game">
             <input
             type="number"
-            
+            value ={this.state.number}
             />
-               <button>provar</button>
-               <p>Texto</p>
+               <button onClick={this.handleOnClick}>probar</button>
+               <p>{this.state.message}</p>
             </div>
         );
     }
@@ -29,4 +71,10 @@ export default Game;
 
 function generateRandomNumber(max, min=1) {
     return Math.floor(Math.random()*(max - min)+ min);
+}
+
+function calculateText(numver, random){
+    if (number == random){
+        return "felicidades has acertado"
+    }
 }
